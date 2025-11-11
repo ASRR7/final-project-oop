fec82a73315b3d72a27749f7d77fadfa21aa17fb
Ivan <andreey609@gmail.com>
2025-11-10 22:33:45 -0600

    Modificacion a Habitacion.java

--- hospital/object/recursos/Habitacion.java
+++ hospital/object/recursos/Habitacion.java
@@ -5,4 +5,37 @@ import hospital.state.IHabitacionState;
 public class Habitacion {
     private IHabitacionState estado;
     
+    private int piso;
+    private int pasillo;
+    private int numCamas;
+
+    public Habitacion(int piso, int pasillo, int numCamas) {
+        this.piso = piso;
+        this.pasillo = pasillo;
+        this.numCamas = numCamas;
+    }
+
+    public int getPiso() {
+        return piso;
+    }
+
+    public void setPiso(int piso) {
+        this.piso = piso;
+    }
+
+    public int getPasillo() {
+        return pasillo;
+    }
+
+    public void setPasillo(int pasillo) {
+        this.pasillo = pasillo;
+    }
+
+    public int getNumCamas() {
+        return numCamas;
+    }
+
+    public void setNumCamas(int numCamas) {
+        this.numCamas = numCamas;
+    }
 }
