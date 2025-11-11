9693b7a2d835f87513c5bdf3b45816cce0762a2a
unknown <julioserrepe1@gmail.com>
2025-11-10 22:39:43 -0600

    Agregando atributos, constructores, getters y setters de Admin

--- hospital/object/usuarios/Admin.java
+++ hospital/object/usuarios/Admin.java
@@ -1,5 +1,19 @@
 package hospital.object.usuarios;
 
 public class Admin extends Usuario {
-    
+    private static int numAdminHist = 0;
+    private static int numAdminActual = 0;
+
+    public Admin( String nombre, String contrasena, double sueldo, String turno ) {
+        super(nombre, contrasena, sueldo, turno); 
+        numAdminHist++;
+        numAdminActual++;
+    }
+
+    public static int getNumAdminHist() {
+        return numAdminHist;
+    }
+    public static int getNumAdminActual() {
+        return numAdminActual;
+    }
 }
