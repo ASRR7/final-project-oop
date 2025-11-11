b120217c1413946b73461d2966d7a9d574635987
Ivan <andreey609@gmail.com>
2025-11-10 20:53:04 -0600

    Hola

--- hospital/object/usuarios/Doctor.java
+++ hospital/object/usuarios/Doctor.java
@@ -1,10 +1,8 @@
 package hospital.object.usuarios;
 
-import hospital.object.pacientes.Paciente;
-
 public class Doctor extends Usuario {
     private double sueldo; 
     private String especialidad; 
     private String turno; 
-    private Array <Paciente>; 
+
 }
