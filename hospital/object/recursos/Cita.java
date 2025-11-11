1b7dd88d36e3732da83c0456685bc418b680ecc6
Ivan <andreey609@gmail.com>
2025-11-10 22:31:05 -0600

    Modificacion a citas

--- hospital/object/recursos/Cita.java
+++ hospital/object/recursos/Cita.java
@@ -1,7 +1,62 @@
 package hospital.object.recursos;
 
 import hospital.state.ICitaState;
+import hospital.object.usuarios.Doctor; 
 
 public class Cita {
     private ICitaState estado;
+
+    private double hora;
+    private int dia;
+    private int mes;
+    private Doctor doctor;
+    private int consultorio;
+
+    public Prueba(double hora, int dia, int mes, Doctor doctor, int consultorio) {
+        this.hora = hora;
+        this.dia = dia;
+        this.mes = mes;
+        this.doctor = doctor;
+        this.consultorio = consultorio;
+    }
+
+    public double getHora() {
+        return hora;
+    }
+
+    public void setHora(double hora) {
+        this.hora = hora;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public int getConsultorio() {
+        return consultorio;
+    }
+
+    public void setConsultorio(int consultorio) {
+        this.consultorio = consultorio;
+    }
 }
