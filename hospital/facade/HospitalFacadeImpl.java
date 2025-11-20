2fbd6d61e76d6f2b025023ccb78e1be9548a6cd9
Ivan <andreey609@gmail.com>
2025-11-19 23:55:56 -0600

    Fachada para administrador

--- hospital/facade/HospitalFacadeImpl.java
+++ hospital/facade/HospitalFacadeImpl.java
@@ -15,4 +15,23 @@ public class HospitalFacadeImpl {
         } while (opcion < 1 || opcion > n);
         return opcion;
     }
+    public void administrador (Scanner sc) {
+        int opcionAdmin;
+        do {
+            opcionAdmin = desplegarMenu("Menu Administrador\n1. Contratar Doctor\n2. Despedir Doctor\n3. Ver Reportes\n4. Salir\nIngrese una opcion: ", 4, sc);
+            switch (opcionAdmin) {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    System.out.println("Saliendo del menú de administrador...");
+                    break;
+            }
+        } while (opcionAdmin != 4);
+    }
 }
