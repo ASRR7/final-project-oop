ca334cb6888e38eeac26e2b1c5a657cedb603ed4
caSkds <cesar.hidalgo.rdz@comunidad.unam.mx>
2025-11-19 23:57:26 -0600

    Changed fachada methods

--- hospital/facade/HospitalFacadeImpl.java
+++ hospital/facade/HospitalFacadeImpl.java
@@ -2,6 +2,10 @@ package hospital.facade;
 
 import java.util.Scanner;
 
+import hospital.object.usuarios.Admin;
+import hospital.object.usuarios.Doctor;
+import hospital.object.usuarios.Recepcionista;
+
 public class HospitalFacadeImpl {
     public static int desplegarMenu(String texto, int n, Scanner sc) {
         int opcion;
@@ -34,4 +38,96 @@ public class HospitalFacadeImpl {
             }
         } while (opcionAdmin != 4);
     }
+        public static Doctor makeDoctor() {
+
+
+        String nombre = voidStringValidator("Ingrese el nombre del doctor: ");
+
+        String contrasena =voidStringValidator("Ingrese la contraseña del doctor: ");
+
+        double sueldo = doubleValidator("Ingrese el sueldo del doctor: ");
+
+        String turno = voidStringValidator("Ingrese el turno del doctor: ");
+
+        String especialidad = voidStringValidator("Ingrese la especialidad del doctor:");
+
+        return new Doctor(nombre, contrasena, sueldo, turno, especialidad);
+    }
+
+    public static Admin makeAdmin() {
+
+        String nombre = voidStringValidator("Ingrese el nombre del administrador:");
+
+
+        String contrasena = voidStringValidator("Ingrese la contraseña del administrador:");
+
+
+        double sueldo = doubleValidator("Ingrese el sueldo del administrador:");
+
+
+        String turno = voidStringValidator("Ingrese el turno del administrador:");
+
+        return new Admin(nombre, contrasena, sueldo, turno);
+    }
+    public static Recepcionista makeRecepcionista() {
+
+        String nombre = voidStringValidator("Ingrese el nombre del administrador:");
+
+
+        String contrasena = voidStringValidator("Ingrese la contraseña del administrador:");
+
+
+        double sueldo = doubleValidator("Ingrese el sueldo del administrador:");
+
+
+        String turno = voidStringValidator("Ingrese el turno del administrador:");
+
+        return new Recepcionista(nombre, contrasena, sueldo, turno);
+    }
+
+    
+    public static int integerValidator(String prompt){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(prompt);
+        int option;
+        try{
+            option = scanner.nextInt();
+            scanner.close();
+            return option;
+        } catch (Exception e) {
+            System.out.println("Favor de ingresar un número entero");
+            return integerValidator(prompt);
+        }
+    }
+    public static double doubleValidator(String prompt){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(prompt);
+        double option;
+        try{
+            option = scanner.nextDouble();
+            scanner.close();
+            return option;
+        } catch (Exception e) {
+            System.out.println("Favor de ingresar un número");
+            return doubleValidator(prompt);
+        }
+    }
+
+    public static String voidStringValidator(String prompt){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(prompt);
+        String option;
+        try{
+            option = scanner.nextLine();
+            scanner.close();
+            if (option.isEmpty()) {
+                throw new IllegalArgumentException();
+            }
+            return option;
+        } catch (Exception e) {
+            System.out.println("El campo no puede estar vacio");
+            return voidStringValidator(prompt); 
+        }
+    }
+
 }
