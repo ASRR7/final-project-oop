31d3398129cae99bdc4e20b237a7ac6862b74c7d
Ivan <andreey609@gmail.com>
2025-11-20 23:42:38 -0600

    Hola

--- hospital/servicios/ReporteServicio.java
+++ hospital/servicios/ReporteServicio.java
@@ -1,5 +1,25 @@
 package hospital.servicios;
 
+import hospital.files.reportes.*; 
+import hospital.object.recursos.*;
+import hospital.object.usuarios.*;
+import hospital.object.pacientes.*;
+import java.util.ArrayList;
+
 public class ReporteServicio {
-    public static void crearReporte
+    public static void crearReporteCita(ArrayList<Cita> citas){
+        CitaReporte.makeCsvReport(citas);
+    }
+
+    public static void crearReporteDoctor(ArrayList<Doctor> doctores){
+        DoctorReporte.makeCsvReport(doctores);
+    }
+
+    public static void crearReporteHabitacion(ArrayList<Habitacion> habitaciones){
+        HabitacionReporte.makeCsvReport(habitaciones);
+    }
+
+    public static void crearReportePaciente(ArrayList<Paciente> pacientes){
+        PacienteReporte.makeCsvReport(pacientes);
+    }
 }
