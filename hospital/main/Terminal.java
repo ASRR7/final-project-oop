41f847d190009d84f2189cbde0c22dccd99a5d47
unknown <julioserrepe1@gmail.com>
2025-11-20 22:35:26 -0600

    Métodos de la clase terminal

--- hospital/main/Terminal.java
+++ hospital/main/Terminal.java
@@ -2,6 +2,8 @@ package hospital.main;
 
 import java.util.Scanner;
 
+import javax.print.Doc;
+
 import hospital.object.pacientes.*;
 import hospital.object.usuarios.*;
 import hospital.state.*;
@@ -10,6 +12,8 @@ import hospital.facade.*;
 
 public class Terminal {
     HospitalFacadeImpl facade;
+    Paciente paciente;
+    Doctor doctor;
     Scanner scanner = new Scanner(System.in);
 
     public Terminal(HospitalFacadeImpl facade){
@@ -88,11 +92,16 @@ public class Terminal {
     public void mostrarMenuPaciente(){
         System.out.println("=== Menú Paciente ===");
         System.out.println("1. Registrarte como paciente");
-        System.out.println("2. Pedir cita médica");
-        System.out.println("3. Cancelar cita médica");
-        System.out.println("4. Ver historial médico");
-        System.out.println("5. Ir a consulta");
-        System.out.println("6. Volver al menú principal");
+        System.out.println("2. Ya soy paciente");
+        System.out.println("3. Volver al menú principal");
+    }
+    public void mostrarMenuPacienteInterno(){
+        System.out.println("=== Menú Interno de Paciente ===");
+        System.out.println("1. Pedir cita médica");
+        System.out.println("2. Cancelar cita médica");
+        System.out.println("3. Ver historial médico");
+        System.out.println("4. Volver al menú de paciente");
+
     }
     public void mostrarMenuRecepcionista(){
         System.out.println("=== Menú Recepcionista ===");
@@ -104,8 +113,10 @@ public class Terminal {
         System.out.println("=== Menú Admin ===");
         System.out.println("1. Agregar Doctor");
         System.out.println("2. Remover Doctor");
-        System.out.println("3. Agregar Medicamentos");
-        System.out.println("4. Volver al menú principal");
+        System.out.println("3. Ver medicamentos");
+        System.out.println("4. Agregar Medicamentos");
+        System.out.println("5. Ver Reportes");
+        System.out.println("6. Volver al menú principal");
     }
     public void mostrarMenuDoctor(){
         System.out.println("=== Menú Doctor ===");
@@ -119,30 +130,75 @@ public class Terminal {
         int opcion = this.leerEntero("Seleccione una opción: ");
         switch (opcion) {
             case 1:
-                this.registrarPaciente(); break;
+                this.registrarPaciente();
+                this.menuPacienteInterno();
+                break;
             case 2:
-                this.pedirCita(); break;
+                this.soyPaciente();
+                this.menuPacienteInterno();
+                break;
             case 3:
-                this.cancelarCita(); break;
-            case 4:
-                this.verHistorialMedico(); break;
-            case 5:
-                this.irAConsulta(); break;
-            case 6:
                 System.out.println("Volviendo al menú principal...");
                 break;
-        
             default: System.out.println("Opción inválida.");
         }
     }
-    public void menuCitas(){
-
+    public void menuPacienteInterno(){
+        this.mostrarMenuPacienteInterno();
+        int opcion = this.leerEntero("Seleccione una opción: ");
+        switch (opcion) {
+            case 1:
+                this.pedirCita(); break;
+            case 2:
+                this.cancelarCita(); break;
+            case 3:
+                this.verHistorialMedico(); break;
+            case 4: 
+                System.out.println("Volviendo al menú de paciente...");
+                    break;
+        }
     }
     public void menuAdmin(){
-
+        this.mostrarMenuAdmin();
+        int opcion = this.leerEntero("Seleccione una opción: ");
+        switch (opcion) {
+            case 1:
+                this.agregarDoctor();
+                break;
+            case 2:
+                this.removerDoctor();
+                break;
+            case 3:
+                this.verMedicamentos();
+                break;
+            case 4:
+                this.agregarMedicamentos();
+                break;
+            case 5:
+                this.verReportes();
+                break;
+            case 6:
+                System.out.println("Volviendo al menú principal...");
+                break;
+            default: System.out.println("Opción inválida.");
+        }
     }
-    public void menuDoctores(){
-
+    public void menuDoctor(){
+        this.soyDoctor();
+        this.mostrarMenuDoctor();
+        int opcion = this.leerEntero("Seleccione una opción: ");
+        switch (opcion) {
+            case 1:
+                this.verCitasAsignadas();
+                break;
+            case 2:
+                this.irAConsulta();
+                break;
+            case 3:
+                System.out.println("Volviendo al menú principal...");
+                break;
+            default: System.out.println("Opción inválida.");
+        }
     }
     
 
@@ -163,10 +219,106 @@ public class Terminal {
         } else if(edad<12){
             esInfancia = true;
         }
-        facade.pacienteServicio.registrarPaciente(nombre, sexo, edad, esEmbarazo, esLactancia, esGeriatria, esInfancia);
+        paciente = facade.pacienteServicio.registrarPaciente(nombre, sexo, edad, esEmbarazo, esLactancia, esGeriatria, esInfancia);
+        System.out.println("Paciente registrado exitosamente.");
     }
+    public void soyPaciente(){
+        int id = leerEntero("Ingrese su ID de paciente: ");
+        paciente = facade.pacienteServicio.obtenerPacientePorId(id);
+        if(paciente != null){
+            System.out.println("Bienvenide, " + paciente.getNombre());
+        } else {
+            System.out.println("Paciente no encontrado.");
+        }
+    }
+    public void pedirCita(){
+        double hora = leerDouble("Ingrese la hora de la cita (formato 24h, ej. 14.30): ");
+        int dia = leerEntero("Ingresa el día del mes para tu cita: ");
+        int mes = leerEntero("Ingresa el número de mes: ");
+        if(facade.citaServicio.pedirCita(paciente.getId(), hora, dia, mes)){
+            System.out.println("Cita pedida exitosamente.");
+        }else {
+            System.out.println("No se pudo agendar la cita.");
+        }
+    }
+    public void cancelarCita(){
+        int idCita = leerEntero("Ingrese el ID de la cita a cancelar: ");
+        if(facade.citaServicio.cancelarCita(paciente.getId(), idCita)){
+            System.out.println("Cita cancelada exitosamente.");
+        }else {
+            System.out.println("No se pudo cancelar la cita.");
+        }
+    }
+    public void verHistorialMedico(){
+        System.out.println("Mostrando historial médico...");
+        System.out.println(facade.pacienteServicio.verHistorialMedico(paciente.getId()));
+    }
+    // =========================================
+    // ADMIN
+    // =========================================
+    public void agregarDoctor(){
+        String nombre = leerTexto("Ingrese el nombre del doctor: ");
+        String contrasena = leerTexto("Ingrese la contraseña del doctor: ");
+        double sueldo = leerDouble("Ingrese el sueldo del doctor: ");
+        String turno = leerTexto("Ingrese el turno del doctor: ");
+        String especialidad = leerTexto("Ingrese la especialidad del doctor: ");
 
-
+        facade.adminServicio.agregarDoctor(nombre, contrasena, sueldo, turno, especialidad);
+        System.out.println("Doctor agregado exitosamente.");
+    }
+    public void removerDoctor(){
+        int id = leerEntero("Ingrese el ID del doctor a remover: ");
+        facade.adminServicio.removerDoctor(id);
+        System.out.println("Doctor removido exitosamente.");
+    }
+    public void verMedicamentos(){
+        System.out.println("Mostrando medicamentos...");
+        System.out.println(facade.medicamentoServicio.verMedicamentos());
+    }
+    public void agregarMedicamentos(){
+        int id = leerEntero("Ingrese el ID del medicamento a agregar: ");
+        int cantidad = leerEntero("Ingrese la cantidad a agregar: ");
+        facade.medicamentoServicio.agregarMedicamento(id, cantidad);
+        System.out.println("Medicamento agregado exitosamente.");
+    }
+    public void verReportes(){
+        System.out.println("Generando reportes...");
+        facade.reporteServicio.generarReportes();
+        System.out.println("Reportes generados exitosamente.");
+    }
+    
+    // =========================================
+    // DOCTOR
+    // =========================================
+    public void soyDoctor(){
+        int id = leerEntero("Ingrese su ID de doctor: ");
+        String contra = leerTexto("Ingresa tu contraseña: ");
+        doctor = facade.doctorServicio.obtenerDoctorPorId(id, contra);
+        if(doctor != null){
+            System.out.println("Bienvenide, Dr. " + doctor.getNombre());
+        } else {
+            System.out.println("Doctor no encontrado.");
+        }
+    }
+    public void irAConsulta(){
+        int id = leerEntero("Ingrese el ID de la cita médica: ");
+        System.out.println("La consulta ha comenzado.");
+        System.out.println("Info de la cita: " + facade.citaServicio.info(id));
+        System.out.println("La consulta ha finalizado: " + facade.doctorServicio.irAConsulta(id));
+    }
+    public void verCitasAsignadas(){
+        int id = leerEntero("Ingrese su ID de doctor: ");
+        Doctor doctor = facade.doctorServicio.obtenerDoctorPorId(id);
+        if(doctor != null){
+            facade.doctorServicio.verCitasAsignadas(doctor);
+        } else {
+            System.out.println("Doctor no encontrado.");
+        }
+    }
+    
+    // -------------------------------------------
+    // -------------------------------------------
+    // -------------------------------------------
     public void cerrar(){
         System.out.println("Cerrando sistema del hospital...");
         facade.cerrarSistema();
