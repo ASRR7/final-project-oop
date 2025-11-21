bddff5b23d6efdb5d7d4177055537b0661457a2e
caSkds <cesar.hidalgo.rdz@comunidad.unam.mx>
2025-11-20 22:03:23 -0600

    Added writing to pacientes.txt

--- hospital/servicios/PacienteServicio.java
+++ hospital/servicios/PacienteServicio.java
@@ -1,5 +1,45 @@
 package hospital.servicios;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import hospital.object.pacientes.Paciente;
+
 public class PacienteServicio {
+    public static void writePacienteTxt(ArrayList<Paciente> Pacientes) {
+        String fileName = "Pacientes.txt";
+        String filePath = "../archivosTXT/" + fileName;
+        String comma = ",";
+        String lineBreak = "\n";
+        try{
+        FileWriter myWriter = new FileWriter(filePath);
+        for(Paciente paciente: Pacientes){
+                    String nombre = paciente.getNombre();
+                    String sexo = paciente.getSexo();
+                    String edad = Integer.toString(paciente.getEdad());
+                    String esEmbarazo = paciente.getEsEmbarazo() ? "Si" : "No";
+                    String esLactancia = paciente.getEsLactancia() ? "Si" : "No";
+                    String esGeriatria = paciente.getEsGeriatria() ? "Si" : "No";
+                    String esInfancia = paciente.getEsInfancia() ? "Si" : "No";
+                    String estado = paciente.toString();
+                    String pacienteInfo = nombre+comma +
+                            sexo +comma +
+                            edad + comma +
+                            estado + comma +
+                            esEmbarazo + comma +
+                            esLactancia + comma +
+                            esGeriatria + comma +
+                            esInfancia + comma +
+                            lineBreak;
+                    myWriter.write(pacienteInfo);
+                }
+                myWriter.close();
+                System.out.println("Los datos de los doctores han sido almacenados correctamente.");
     
+    }catch (IOException e) {
+        System.out.println("Ocurrió un error al almacenar los dotores.");
+        e.printStackTrace();
+    }
+    }
 }
