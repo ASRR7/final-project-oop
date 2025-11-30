60e71043161e4ed596b6ec1dac442f8d83630c97
caSkds <cesar.hidalgo.rdz@comunidad.unam.mx>
2025-11-30 17:02:51 -0600

    Added override tags

--- hospital/state/paciente/EstadoMuerto.java
+++ hospital/state/paciente/EstadoMuerto.java
@@ -2,22 +2,27 @@ package hospital.state.paciente;
 
 import hospital.object.pacientes.Paciente;
 
-public class EstadoMuerto {
+public class EstadoMuerto implements hospital.state.IPacienteState {
     public void estadoMuerto() {
         
     }
+    @Override
     public void agendarCita(){
         System.out.println("El paciente está muerto, no puede agendar citas");
     }; 
+    @Override
     public void cancelarCita(){
         System.out.println("El paciente está muerto, no puede cancelar citas");
     };
+    @Override
     public void irACita(){
         System.out.println("El paciente está muerto, no puede ir a citas");
     };
+    @Override
     public void salirDelHospital(){
         System.out.println("El paciente ha salido del hospital");
     };
+    @Override
     public void setContextoPaciente(Paciente contexto){
             contexto.setEsEmbarazo(Boolean.FALSE);
             contexto.setEsLactancia(Boolean.FALSE);
